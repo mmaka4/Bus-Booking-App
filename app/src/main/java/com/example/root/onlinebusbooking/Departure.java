89c3439ac277bb369547eb72e1b097be04024322
root <saidimmaka@gmail.com>
2019-08-25 02:51:01 +0300

    changed on 26Aug(Bus sheet)

--- app/src/main/java/com/example/root/onlinebusbooking/Departure.java
+++ app/src/main/java/com/example/root/onlinebusbooking/Departure.java
@@ -1,15 +1,185 @@
 package com.example.root.onlinebusbooking;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 //import android.support.v7.app.AppCompatActivity;
 
-public class Departure extends AppCompatActivity {
+public class Departure extends AppCompatActivity implements  View.OnClickListener {
+
+    ViewGroup layout;
+
+    String seats = "UU-RU/UU-AA/AA-AR/RR-UU/"
+            + "AR-UR/UU-AR/RR-AU/UU-RA/"
+            + "UA-UA/RU-AU/UR-RU/AU-RU/"
+            + "AR-UU/RAAAR/";
+
+    List<TextView> seatViewList = new ArrayList<>();
+    int seatSize = 90;
+    int seatGaping = 6;
+
+    int STATUS_AVAILABLE = 1;
+    int STATUS_BOOKED = 2;
+    int STATUS_RESERVED = 3;
+    String selectedIds = "";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.departure_seats);
+
+        layout = findViewById(R.id.layoutSeat);
+
+        seats = "/" + seats;
+
+        LinearLayout layoutSeat = new LinearLayout(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutSeat.setOrientation(LinearLayout.VERTICAL);
+        layoutSeat.setLayoutParams(params);
+        layoutSeat.setPadding(15 * seatGaping, 5 * seatGaping, 12 * seatGaping, 8 * seatGaping);
+        layout.addView(layoutSeat);
+
+        LinearLayout layout = null;
+
+
+        int count = 0;
+        Log.d("Seat Length", " "+ seats.length());
+
+        for (int index = 0; index < seats.length(); index++) {
+            if (seats.charAt(index) == '/') {
+                layout = new LinearLayout(this);
+                layout.setOrientation(LinearLayout.HORIZONTAL);
+                layoutSeat.addView(layout);
+            } else if (seats.charAt(index) == 'U') {
+                count++;
+                Log.d("Count ", " "+ count);
+                TextView view = new TextView(this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                view.setLayoutParams(layoutParams);
+                view.setPadding(0, 0, 0, 2 * seatGaping);
+                view.setId(count);
+                view.setGravity(Gravity.CENTER);
+                view.setBackgroundResource(R.drawable.bus_seat3);
+                view.setTextColor(Color.WHITE);
+                view.setTag(STATUS_BOOKED);
+                view.setText(count + "");
+                view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+                layout.addView(view);
+                seatViewList.add(view);
+                view.setOnClickListener(this);
+            } else if (seats.charAt(index) == 'A') {
+                count++;
+                TextView view = new TextView(this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                view.setLayoutParams(layoutParams);
+                view.setPadding(0, 0, 0, 2 * seatGaping);
+                view.setId(count);
+                view.setGravity(Gravity.CENTER);
+                view.setBackgroundResource(R.drawable.bus_seat3);
+                view.setText(count + "");
+                view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+                view.setTextColor(Color.BLACK);
+                view.setTag(STATUS_AVAILABLE);
+                layout.addView(view);
+                seatViewList.add(view);
+                view.setOnClickListener(this);
+            } else if (seats.charAt(index) == 'R') {
+                count++;
+                TextView view = new TextView(this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                view.setLayoutParams(layoutParams);
+                view.setPadding(0, 0, 0, 2 * seatGaping);
+                view.setId(count);
+                view.setGravity(Gravity.CENTER);
+                view.setBackgroundResource(R.drawable.bus_seat3);
+                view.setText(count + "");
+                view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+                view.setTextColor(Color.WHITE);
+                view.setTag(STATUS_RESERVED);
+                layout.addView(view);
+                seatViewList.add(view);
+                view.setOnClickListener(this);
+            } else if (seats.charAt(index) == '_') {
+                TextView view = new TextView(this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                view.setLayoutParams(layoutParams);
+                view.setBackgroundColor(Color.TRANSPARENT);
+                view.setText("");
+                layout.addView(view);
+            } else {
+                Log.d("Collidor", " "+ index);
+
+                TextView view = new TextView(this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                layoutParams.setMargins(18 *seatGaping, seatGaping, seatGaping, seatGaping);
+                view.setLayoutParams(layoutParams);
+                view.setPadding(0, 0, 0, 2 * seatGaping);
+                view.setId(count++);
+                view.setGravity(Gravity.CENTER);
+
+                view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+
+                if(seats.charAt(index+1) == 'U') {
+                    //Log.d("Collidor", " "+ index);
+                    view.setBackgroundResource(R.drawable.bus_seat3);
+                    view.setTextColor(Color.WHITE);
+                    view.setTag(STATUS_BOOKED);
+                    view.setText(count + "");
+
+                } else if (seats.charAt(index+1) == 'R') {
+                    view.setBackgroundResource(R.drawable.bus_seat3);
+                    view.setText(count + "");
+                    view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+                    view.setTextColor(Color.WHITE);
+                    view.setTag(STATUS_RESERVED);
+
+                } else if (seats.charAt(index+1) == 'A') {
+                    view.setBackgroundResource(R.drawable.bus_seat3);
+                    view.setText(count + "");
+                    view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+                    view.setTextColor(Color.BLACK);
+                    view.setTag(STATUS_AVAILABLE);
+
+                }
+
+                layout.addView(view);
+                seatViewList.add(view);
+                view.setOnClickListener(this);
+                index++;
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if ((int) view.getTag() == STATUS_AVAILABLE) {
+            if (selectedIds.contains(view.getId() + ",")) {
+                selectedIds = selectedIds.replace(+view.getId() + ",", "");
+                view.setBackgroundResource(R.drawable.bus_seat3);
+            } else {
+                selectedIds = selectedIds + view.getId() + ",";
+                view.setBackgroundResource(R.drawable.bus_seat3);
+            }
+        } else if ((int) view.getTag() == STATUS_BOOKED) {
+            Toast.makeText(this, "Seat " + view.getId() + " is Booked", Toast.LENGTH_SHORT).show();
+        } else if ((int) view.getTag() == STATUS_RESERVED) {
+            Toast.makeText(this, "Seat " + view.getId() + " is Reserved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
